88b5c87065e4debda33b08e1e525d1ec03ffe3c1
MoryakSPb <31323043+MoryakSPb@users.noreply.github.com>
2022-07-04 03:56:04 +0300

    Initial commit

---
+++ Member.cs
@@ -0,0 +1,52 @@
+﻿using MessagePack;
+
+namespace SerialMemTest;
+
+[MessagePackObject]
+public record Member
+{
+    [Key(0)]
+    public Guid Id { get; set; }
+
+    [Key(1)]
+    public ulong? DiscordId { get; set; }
+
+    [Key(2)]
+    public ulong? SteamId { get; set; }
+
+    [Key(3)]
+    public long? MicrosoftId { get; set; }
+
+    [Key(4)]
+    public uint VkId { get; set; }
+
+    [Key(5)]
+    public long? TelegramId { get; set; }
+
+    [Key(6)]
+    public string Nickname { get; set; } = string.Empty;
+
+    [Key(7)]
+    public string? RealName { get; set; }
+
+    [Key(8)]
+    public short Rank { get; set; }
+
+    [Key(9)]
+    public bool IsSquadCommander { get; set; }
+
+    [Key(10)]
+    public short? SquadNumber { get; set; }
+
+    [Key(11)]
+    public short SquadPartNumber { get; set; }
+
+    [Key(12)]
+    public DateTime LastChanged { get; set; }
+
+    [Key(13)]
+    public Guid ConcurrencyToken { get; set; }
+
+    [Key(14)]
+    public string? Description { get; set; }
+}
---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using Apex.Serialization;
+using MessagePack;
+using SerialMemTest;
+
+Member member = new()
+{
+    ConcurrencyToken = Guid.NewGuid(),
+    DiscordId = 266672873882648577UL,
+    Id = Guid.NewGuid(),
+    IsSquadCommander = true,
+    LastChanged = DateTime.UtcNow,
+    MicrosoftId = null,
+    Nickname = "MoryakSPb",
+    Rank = 452,
+    RealName = "Александр",
+    SquadNumber = 0,
+    SteamId = 1215,
+    TelegramId = 24352154,
+    VkId = 0,
+    SquadPartNumber = 1,
+    Description = null
+};
+
+long messagePackLength = MessagePackSerializer.Serialize(member).LongLength;
+
+IBinary serializer = Binary.Create(new Settings().MarkSerializable(typeof(Member)));
+await using MemoryStream stream = new();
+serializer.Write(member, stream);
+long apexLength = stream.Position;
+
+Console.WriteLine($"MessagePackLength = {messagePackLength:D5}");
+Console.WriteLine($"ApexLength = {apexLength:D5}");
